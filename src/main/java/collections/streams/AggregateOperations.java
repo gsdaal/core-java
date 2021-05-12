6301722bfd9b67bd1df875c45f27cb5b482dfe04
gsdaal <49695900+gsdaal@users.noreply.github.com>
2021-05-12 11:28:50 +0100

    making local variables as 'var'

--- src/main/java/collections/streams/AggregateOperations.java
+++ src/main/java/collections/streams/AggregateOperations.java
@@ -3,11 +3,10 @@ package collections.streams;
 import collections.streams.model.Person;
 import collections.streams.model.PersonService;
 
-import java.util.List;
 import java.util.stream.Stream;
 
-import static java.lang.System.out;
 import static collections.streams.model.Person.Sex.MALE;
+import static java.lang.System.out;
 
 public class AggregateOperations {
 
